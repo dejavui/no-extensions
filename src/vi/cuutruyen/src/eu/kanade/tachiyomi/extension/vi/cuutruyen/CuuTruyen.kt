1d1117f9474da5d2d3896b3edb9e4e023fb2e53a
are-are-are <62763969+dejavui@users.noreply.github.com>
2026-06-17 00:02:11 +0700

    Update CuuTruyen.kt

--- src/vi/cuutruyen/src/eu/kanade/tachiyomi/extension/vi/cuutruyen/CuuTruyen.kt
+++ src/vi/cuutruyen/src/eu/kanade/tachiyomi/extension/vi/cuutruyen/CuuTruyen.kt
@@ -5,7 +5,6 @@ import android.widget.Toast
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import eu.kanade.tachiyomi.source.ConfigurableSource
 import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
@@ -14,6 +13,7 @@ import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.HttpSource
+import keiyoushi.network.rateLimit
 import keiyoushi.utils.getPreferences
 import keiyoushi.utils.parseAs
 import okhttp3.CacheControl
