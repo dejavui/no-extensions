e6636325180f1e09bec345311159ae502789a3b4
are-are-are <62763969+dejavui@users.noreply.github.com>
2026-06-24 14:29:10 +0700

    Add HentaiVNBond
    Madara theme

---
+++ src/vi/hentaivnbond/src/eu/kanade/tachiyomi/extension/vi/hentaivnbond/HentaiVNBond.kt
@@ -0,0 +1,63 @@
+package eu.kanade.tachiyomi.extension.vi.hentaivnbond
+
+import android.content.SharedPreferences
+import android.widget.Toast
+import androidx.preference.PreferenceScreen
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.source.ConfigurableSource
+import keiyoushi.utils.getPreferences
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class HentaiVNBond :
+    Madara(
+        "HentaiVNBond",
+        "https://hentaivn.bond",
+        "vi",
+        dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ROOT),
+    ),
+    ConfigurableSource {
+
+    override val mangaSubString = "truyenhentai"
+
+    private val preferences: SharedPreferences = getPreferences {
+        getString(DEFAULT_BASE_URL_PREF, null).let { prefDefaultBaseUrl ->
+            if (prefDefaultBaseUrl != super.baseUrl) {
+                edit()
+                    .putString(BASE_URL_PREF, super.baseUrl)
+                    .putString(DEFAULT_BASE_URL_PREF, super.baseUrl)
+                    .apply()
+            }
+        }
+    }
+
+    override val baseUrl get() = getPrefBaseUrl()
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        val baseUrlPref = androidx.preference.EditTextPreference(screen.context).apply {
+            key = BASE_URL_PREF
+            title = BASE_URL_PREF_TITLE
+            summary = BASE_URL_PREF_SUMMARY
+            setDefaultValue(super.baseUrl)
+            dialogTitle = BASE_URL_PREF_TITLE
+            dialogMessage = "Default: ${super.baseUrl}"
+
+            setOnPreferenceChangeListener { _, _ ->
+                Toast.makeText(screen.context, RESTART_APP, Toast.LENGTH_LONG).show()
+                true
+            }
+        }
+        screen.addPreference(baseUrlPref)
+    }
+
+    private fun getPrefBaseUrl(): String = preferences.getString(BASE_URL_PREF, super.baseUrl)!!
+
+    companion object {
+        private const val DEFAULT_BASE_URL_PREF = "defaultBaseUrl"
+        private const val RESTART_APP = "Khởi chạy lại ứng dụng để áp dụng thay đổi."
+        private const val BASE_URL_PREF_TITLE = "Ghi đè URL cơ sở"
+        private const val BASE_URL_PREF = "overrideBaseUrl"
+        private const val BASE_URL_PREF_SUMMARY =
+            "Dành cho sử dụng tạm thời, cập nhật tiện ích sẽ xóa cài đặt."
+    }
+}
