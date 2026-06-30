4b5f125d442a7f63e55518d571d7a9145ff8e1d1
are-are-are <62763969+dejavui@users.noreply.github.com>
2026-07-01 02:32:02 +0700

    EHentai to new dsl

--- src/all/ehentai/build.gradle.kts
+++ src/all/ehentai/build.gradle.kts
@@ -4,10 +4,23 @@ plugins {
 
 keiyoushi {
     name = "E-Hentai"
-    className = "EHFactory"
     versionCode = 27
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
+
+    val languages = listOf(
+        "ja", "en", "zh", "nl", "fr", "de", "hu", "it", "ko", "pl", "pt-BR", "ru", "es", "th", "vi", "none", "other",
+    )
+
+    languages.forEach {
+        source {
+            lang = it
+            baseUrl = "https://e-hentai.org"
+            if (it == "pt-BR") {
+                id = 7151438547982231541L
+            }
+        }
+    }
 
     deeplink {
         host("e-hentai.org")
