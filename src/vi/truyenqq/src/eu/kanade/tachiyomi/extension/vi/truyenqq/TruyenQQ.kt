917f3c52bcb710a9c7bd2521b550b6c4673e7e33
are-are-are <62763969+dejavui@users.noreply.github.com>
2026-06-30 20:15:05 +0700

    merge

--- src/vi/truyenqq/src/eu/kanade/tachiyomi/extension/vi/truyenqq/TruyenQQ.kt
+++ src/vi/truyenqq/src/eu/kanade/tachiyomi/extension/vi/truyenqq/TruyenQQ.kt
@@ -1,9 +1,5 @@
 package eu.kanade.tachiyomi.extension.vi.truyenqq
 
-import android.content.SharedPreferences
-import android.widget.Toast
-import androidx.preference.EditTextPreference
-import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.ConfigurableSource
 import eu.kanade.tachiyomi.source.model.Filter
@@ -16,7 +12,6 @@ import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.util.asJsoup
 import keiyoushi.annotation.Source
 import keiyoushi.network.rateLimit
-import keiyoushi.utils.getPreferences
 import keiyoushi.utils.tryParse
 import okhttp3.CacheControl
 import okhttp3.Headers
@@ -36,8 +31,6 @@ abstract class TruyenQQ :
     private val baseUrlHost by lazy { baseUrl.toHttpUrl().host }
 
     override val supportsLatest: Boolean = true
-
-    private val preferences: SharedPreferences = getPreferences()
 
     override val client: OkHttpClient = network.client.newBuilder()
         .rateLimit(1, 2.seconds) { it.host == baseUrlHost }
