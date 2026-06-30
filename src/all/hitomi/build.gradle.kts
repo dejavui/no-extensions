2ae44e72d03f76f170df96ce2c987169724ee8ee
are-are-are <62763969+dejavui@users.noreply.github.com>
2026-07-01 02:32:15 +0700

    Hitomi to new dsl

--- src/all/hitomi/build.gradle.kts
+++ src/all/hitomi/build.gradle.kts
@@ -4,8 +4,19 @@ plugins {
 
 keiyoushi {
     name = "Hitomi"
-    className = "HitomiFactory"
     versionCode = 41
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
+
+    val languages = listOf(
+        "all", "en", "id", "jv", "ca", "ceb", "cs", "da", "de", "et", "es", "eo",
+        "fr", "it", "hi", "hu", "pl", "pt", "vi", "tr", "ru", "uk", "ar", "ko", "zh", "ja",
+    )
+
+    languages.forEach {
+        source {
+            lang = it
+            baseUrl = "https://hitomi.la"
+        }
+    }
 }
--- src/all/hitomi/src/eu/kanade/tachiyomi/extension/all/hitomi/Hitomi.kt
+++ src/all/hitomi/src/eu/kanade/tachiyomi/extension/all/hitomi/Hitomi.kt
@@ -10,6 +10,7 @@ import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.model.UpdateStrategy
 import eu.kanade.tachiyomi.source.online.HttpSource
+import keiyoushi.annotation.Source
 import keiyoushi.utils.parseAs
 import keiyoushi.utils.tryParse
 import kotlinx.coroutines.async
@@ -37,13 +38,43 @@ import java.util.Locale
 import kotlin.math.min
 import kotlin.time.Duration.Companion.seconds
 
+@Source
 @OptIn(ExperimentalUnsignedTypes::class)
-class Hitomi(
-    override val lang: String,
-    private val nozomiLang: String,
-) : HttpSource() {
+abstract class Hitomi : HttpSource() {
 
     override val name = "Hitomi"
+
+    private val nozomiLang by lazy {
+        when (lang) {
+            "all" -> "all"
+            "en" -> "english"
+            "id" -> "indonesian"
+            "jv" -> "javanese"
+            "ca" -> "catalan"
+            "ceb" -> "cebuano"
+            "cs" -> "czech"
+            "da" -> "danish"
+            "de" -> "german"
+            "et" -> "estonian"
+            "es" -> "spanish"
+            "eo" -> "esperanto"
+            "fr" -> "french"
+            "it" -> "italian"
+            "hi" -> "hindi"
+            "hu" -> "hungarian"
+            "pl" -> "polish"
+            "pt" -> "portuguese"
+            "vi" -> "vietnamese"
+            "tr" -> "turkish"
+            "ru" -> "russian"
+            "uk" -> "ukrainian"
+            "ar" -> "arabic"
+            "ko" -> "korean"
+            "zh" -> "chinese"
+            "ja" -> "japanese"
+            else -> lang
+        }
+    }
 
     private val cdnDomain = "gold-usergeneratedcontent.net"
 
