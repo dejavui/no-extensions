3dd4ab804201bdd0fcf49d231bf6dead8001d527
are-are-are <62763969+dejavui@users.noreply.github.com>
2026-08-07 04:00:48 +0700

    Add HentaiVNIsReal

---
+++ src/vi/hentaivnisreal/build.gradle.kts
@@ -0,0 +1,21 @@
+import io.github.keiyoushi.gradle.api.ContentWarning
+
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "HentaiVN Is Real"
+    versionCode = 1
+    contentWarning = ContentWarning.NSFW
+    libVersion = "1.6"
+
+    source {
+        baseUrl = "https://hentaivnreal.com"
+        lang = "vi"
+    }
+
+    deeplink {
+        path("/truyen/..*")
+    }
+}
