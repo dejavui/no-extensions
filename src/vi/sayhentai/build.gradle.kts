1ac0b459823bf8d5557858fafc0a6d9e0deacd69
are-are-are <62763969+dejavui@users.noreply.github.com>
2026-08-10 18:44:43 +0700

    SayHentai: Fix missing chapter

--- src/vi/sayhentai/build.gradle.kts
+++ src/vi/sayhentai/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 keiyoushi {
     name = "SayHentai"
-    versionCode = 18
+    versionCode = 19
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
     theme = "manhwaz"
--- src/vi/sayhentai/src/eu/kanade/tachiyomi/extension/vi/sayhentai/SayHentai.kt
+++ src/vi/sayhentai/src/eu/kanade/tachiyomi/extension/vi/sayhentai/SayHentai.kt
@@ -1,7 +1,13 @@
 package eu.kanade.tachiyomi.extension.vi.sayhentai
 
 import eu.kanade.tachiyomi.multisrc.manhwaz.ManhwaZ
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.asObservableSuccess
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
 import keiyoushi.annotation.Source
+import org.jsoup.Jsoup
+import rx.Observable
 
 @Source
 abstract class SayHentai : ManhwaZ() {
@@ -11,4 +17,27 @@ abstract class SayHentai : ManhwaZ() {
     override val mangaDetailsStatusHeading = "Trạng thái"
 
     override fun popularMangaSelector() = "#slide-top > .item:contains(a)"
+
+    override fun fetchChapterList(manga: SManga): Observable<List<SChapter>> = client.newCall(chapterListRequest(manga))
+        .asObservableSuccess()
+        .flatMap { response ->
+            val html = response.peekBody(Long.MAX_VALUE).string()
+            val document = Jsoup.parse(html, response.request.url.toString())
+
+            val chapters = super.chapterListParse(response)
+            val moreChaptersUrl = document
+                .selectFirst(".c-chapter-readmore")
+                ?.attr("abs:data-ajax-url")
+                ?.takeIf(String::isNotBlank)
+
+            if (moreChaptersUrl != null) {
+                client.newCall(GET(moreChaptersUrl, headers))
+                    .asObservableSuccess()
+                    .map { moreResponse ->
+                        chapters + super.chapterListParse(moreResponse)
+                    }
+            } else {
+                Observable.just(chapters)
+            }
+        }
 }
